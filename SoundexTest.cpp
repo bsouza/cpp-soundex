2089488e4737e0cf0363a5d44593d89f9b2f7144
bsouza <to.bsouza@gmail.com>
2014-02-24 02:15:52 -0300

    Refactoring dos testes para utilização de fixtures (do GMock)

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -3,15 +3,34 @@
 class Soundex {
 
 public:
-	std::string encode(const std::string& word) const {
-		return "A";
+	std::string encode(const std::string &word) const {
+		return zeroPad(word);
+	}
+
+private:
+
+	std::string zeroPad(const std::string &word) const {
+		return word + "000";
 	}
 
 };
 
-TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
-	Soundex soundex;
-	auto encoded = soundex.encode("A");
+using ::testing::Eq;
 
-	ASSERT_THAT(encoded, testing::Eq("A"));
+class SoundexEncoding: public testing::Test {
+
+public:
+	Soundex soundex;
+
+};
+
+TEST_F(SoundexEncoding, retains_sole_letter_of_one_letter_word) {
+	auto encoded = soundex.encode("A");
+	ASSERT_THAT(encoded, Eq("A000"));
+}
+
+TEST_F(SoundexEncoding, pads_with_zeros_to_ensure_three_digits) {
+	auto encoded = soundex.encode("I");
+
+	ASSERT_THAT(encoded, Eq("I000"));
 }
