d12da4ab1683230c7917da5d4b8fc10f02877826
bsouza <to.bsouza@gmail.com>
2014-02-24 01:21:15 -0300

    - Pequena evolução nos testes;  - Adição de um script rake para ajudar na compilação e execução dos testes;

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -1,8 +1,17 @@
-class Soundex {
-};
-
 #include "gmock/gmock.h"
+
+class Soundex {
+
+public:
+	std::string encode(const std::string& word) const {
+		return "A";
+	}
+
+};
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
 	Soundex soundex;
+	auto encoded = soundex.encode("A");
+
+	ASSERT_THAT(encoded, testing::Eq("A"));
 }
