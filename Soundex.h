a8ed0993ee8de4a7665cb34a841c084a703b3541
bsouza <to.bsouza@gmail.com>
2014-03-03 02:59:10 -0300

    Refactor

--- Soundex.h
+++ Soundex.h
@@ -2,24 +2,86 @@
 #define SOUNDEX_H_
 
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
 class Soundex {
 
+	static const size_t MaxCodeLength{4};
+	const string NotADigit{"*"};
+
 public:
 	string encode(const string &word) const {
-		auto encoded = word.substr(0, 1);
-
-		if (word.length() > 1)
-			encoded += "1";
-
-		return zeroPad(encoded);
+		return zeroPad(upperFront(head(word)) + tail(encodedDigits(word)));
 	}
 
 private:
+	string head(const string &word) const {
+		return word.substr(0,1);
+	}
+
+	string tail(const string &word) const {
+		return word.substr(1);
+	}
+
+	string upperFront(const string &word) const {
+		return string(1, toupper(static_cast<unsigned char>(word.front())));
+	}
+
+	string encodedDigits(const string &word) const {
+		string encoding;
+		encodedHead(encoding, word);
+		encodedTail(encoding, word);
+		return encoding;
+	}
+
+	void encodedHead(string &encoding, const string &word) const {
+		encoding += encodedDigit(word.front());
+	}
+
+	void encodedTail(string &encoding, const string &word) const {
+		for (auto letter : tail(word)) {
+			if (!isComplete(encoding))
+				encodedLetter(encoding, letter);
+		}
+	}
+
+	void encodedLetter(string &encoding, char letter) const {
+		auto digit = encodedDigit(letter);
+		if (digit != NotADigit && digit != lastDigit(encoding))
+			encoding += digit;
+	}
+
+	bool isComplete(const string &word) const {
+		return word.length() == MaxCodeLength;
+	}
+
+	string encodedDigit(char letter) const {
+		const unordered_map<char, string> encodings {
+			{'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
+			{'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"}, {'s', "2"}, {'x', "2"}, {'z', "2"},
+			{'d', "3"}, {'t', "3"},
+			{'l', "4"},
+			{'m', "5"}, {'n', "5"},
+			{'r', "6"}
+		};
+
+		auto it = encodings.find(lower(letter));
+		return (it == encodings.end()) ? NotADigit : it->second;
+	}
+
+	char lower(char letter) const {
+		return tolower(static_cast<unsigned char>(letter));
+	}
+
+	string lastDigit(const string &encoding) const {
+		if (encoding.empty()) return NotADigit;
+		return string(1, encoding.back());
+	}
+
 	string zeroPad(const string &word) const {
-		auto zerosNeeded = 4 - word.length();
+		auto zerosNeeded = MaxCodeLength - word.length();
 		return word + string(zerosNeeded, '0');
 	}
 };
--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -18,6 +18,38 @@ TEST_F(SoundexEncoding, pads_with_zeros_to_ensure_three_digits) {
 	ASSERT_THAT(soundex.encode("I"), Eq("I000"));
 }
 
-TEST_F(SoundexEncoding, replaces_b_with_digit_1) {
-	ASSERT_THAT(soundex.encode("Ab"), Eq("A100"));
+TEST_F(SoundexEncoding, replaces_consonants_with_appropriate_digits) {
+	ASSERT_THAT(soundex.encode("Ax"), Eq("A200"));
+}
+
+TEST_F(SoundexEncoding, ignores_non_alphabetics) {
+	ASSERT_THAT(soundex.encode("A#"), Eq("A000"));
+}
+
+TEST_F(SoundexEncoding, replaces_multiple_consonants_with_digits) {
+	ASSERT_THAT(soundex.encode("Acdl"), Eq("A234"));
+}
+
+TEST_F(SoundexEncoding, limits_length_to_four_characters) {
+	ASSERT_THAT(soundex.encode("Dcdlb").length(), Eq(4u));
+}
+
+TEST_F(SoundexEncoding, ignores_vowel_like_letters) {
+	ASSERT_THAT(soundex.encode("BaAeEiIoOuUhHyYcdl"), Eq("B234"));
+}
+
+TEST_F(SoundexEncoding, combines_duplicate_encoding) {
+	ASSERT_THAT(soundex.encode("Abfcgdt"), Eq("A123"));
+}
+
+TEST_F(SoundexEncoding, uppercases_first_letter) {
+	ASSERT_THAT(soundex.encode("abcd"), StartsWith("A"));
+}
+
+TEST_F(SoundexEncoding, ignores_case_when_encoding_consonants) {
+	ASSERT_THAT(soundex.encode("BCDL"), Eq(soundex.encode("Bcdl")));
+}
+
+TEST_F(SoundexEncoding, combines_duplicate_codes_when_2nd_letter_duplicates_1st) {
+	ASSERT_THAT(soundex.encode("Bbcd"), Eq("B230"));
 }
